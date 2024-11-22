2a22126a3633e6cfa7cdf5152ec59c464d935916
juusokasperi <juusor@gmail.com>
2024-11-22 20:30:12 +0200

    Fixed norm error

--- myLibft/srcs/libft/ft_ltoa.c
+++ myLibft/srcs/libft/ft_ltoa.c
@@ -6,11 +6,11 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 18:17:53 by jrinta-           #+#    #+#             */
-/*   Updated: 2024/11/22 18:21:05 by jrinta-          ###   ########.fr       */
+/*   Updated: 2024/11/22 20:00:25 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "libft.h"
+#include "libft.h"
 
 char	*ft_ltoa(long n)
 {
