43ba94c54ca95d1c755e838207590d5a263596e9
jrinta- <jrinta-@c2r3p10.hive.fi>
2024-11-30 17:19:32 +0200

    Working on ideas for better gnl implementation

--- dump/gnl_linked_list_version/gnl_static_notes.c
+++ dump/gnl_linked_list_version/gnl_static_notes.c
@@ -19,34 +19,51 @@ while (nb_read > 0 && !ft_strchr(buffer, '\n'))
 	nb_read = read(fd, buffer, BUFFER_SIZE);
 	if (nb_read <= 0)
 		break ;
-	new_line = append(new_line, buffer);
-	if (!new_line)
+	line = append(line, buffer);
+	if (!line)
 		break ;
 if (nb_read == -1)
 {
 	clean_buffer(buffer);
-	return (free(&newline));
+	return (free(&line));
 }
 else
-	return (new_line);
+	return (line);
 
-append(new_line, buffer)
+append(line, buffer)
 {
 	char *joined_string;
 	int	i;
 	int	j;
-	if (!new_line)
-		new_line = ft_calloc(1,1);
-		if (!new_line)
+	if (!line)
+		line = ft_calloc(1,1);
+		if (!line)
 			return (NULL);
-	i = ft_strlen(new_line);
-	while (buffer[i])
-		if (buffer[i] == '\n')
-			i++;
+	i = ft_strlen(line);
+	j = 0;
+	while (buffer[j])
+		if (buffer[j] == '\n')
+			j++;
 			break ;
-		i++;
-
+		j++;
+	joined_string = ft_memcpy(joined_string, line, i);
+	joined_string = ft_memcpy(joined_string + i, buffer, j);
+	ft_free(&line);
+	return (joined_string);
 }
 
--> jos /n
--> append
+clean_buffer(buffer)
+{
+	int i;
+
+	i = 0;
+	while (buffer[i] && i < BUFFER_SIZE)
+		if (buffer[i] == '\n')
+			i++;
+			break;
+		i++;
+	ft_memmove(buffer + i, buffer, length?);
+}
+
+enta jos bufferiin luettu vahemman kuin buffer_size? pitasko buffer tayttaa aina nollilla?
+
