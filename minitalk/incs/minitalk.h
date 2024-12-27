45a7c43a47be10266703261fa88ab5aed76f673e
juusokasperi <juusor@gmail.com>
2024-12-27 17:00:49 +0200

    Longer DELAY_TIME

--- minitalk/incs/minitalk.h
+++ minitalk/incs/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 19:36:39 by jrinta-           #+#    #+#             */
-/*   Updated: 2024/12/27 16:12:02 by jrinta-          ###   ########.fr       */
+/*   Updated: 2024/12/27 16:59:58 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <signal.h>
 # include <stdio.h>
 
-# define DELAY 10000
+# define DELAY 50000
 # define RETRY_LIMIT 5
 # define BUFFER_SIZE 1024
 
