7e507bb99fca1d4f65139b2c116cb8ec84255050
juusokasperi <juusor@gmail.com>
2025-01-07 20:54:05 +0200

    Moved old version of FdF to dump

---
+++ dump/FdF/srcs/init/init.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 01:38:23 by jrinta-           #+#    #+#             */
+/*   Updated: 2025/01/04 16:45:22 by jrinta-          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	init_variables(t_info *data)
+{
+	data->width = 0;
+	data->height = 0;
+	data->xyz = NULL;
+	data->colors = NULL;
+	data->zoom = 20;
+	data->highest_z = 0;
+	data->lowest_z = 0;
+	data->z_scale = 1;
+	data->distance = 0.0;
+	data->projection = 1;
+	data->angle = 0.523599;
+	data->default_colors = 1;
+	data->rotate = 0;
+}
+
+void	set_altitude(t_info *data)
+{
+	if (ft_abs(data->highest_z) + ft_abs(data->lowest_z) > 50)
+		data->z_scale = 0.8;
+	else if (ft_abs(data->highest_z) + ft_abs(data->lowest_z) > 25)
+		data->z_scale = 2;
+	else
+		data->z_scale = 5;
+}
+
+int	init_data(t_info *data)
+{
+	init_variables(data);
+	data->mlx_ptr = mlx_init(WIDTH, HEIGHT, "FdF jrinta-42", false);
+	data->mlx_img = mlx_new_image(data->mlx_ptr, WIDTH, HEIGHT);
+	if (!data->mlx_ptr || !data->mlx_img
+		|| (mlx_image_to_window(data->mlx_ptr, data->mlx_img, 0, 0) < 0))
+	{
+		free(data);
+		return (0);
+	}
+	clear_image(data);
+	draw_instructions(data);
+	return (1);
+}
---
+++ dump/FdF/srcs/read_map/read_map_utils.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_map_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 15:33:35 by jrinta-           #+#    #+#             */
+/*   Updated: 2024/12/17 19:49:20 by jrinta-          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+// Tarviiko vielä error handlingin jos esim get_next_line palauttaa NULL heti?
+static uint32_t	hex_to_rgba(const char *hex);
+static int		free_split(char **split);
+
+int	fill_xyz(int *z, char *line, t_info *data)
+{
+	char	**split;
+	int		i;
+
+	i = 0;
+	split = ft_split(line, ' ');
+	if (!split)
+		return (0);
+	while (split[i])
+	{
+		z[i] = ft_atoi(split[i]);
+		if (z[i] > data->highest_z)
+			data->highest_z = z[i];
+		if (z[i] < data->lowest_z)
+			data->lowest_z = z[i];
+		i++;
+	}
+	free_split(split);
+	return (1);
+}
+
+int	fill_colors(uint32_t *colors, char *line, t_info *data)
+{
+	char		**split;
+	char		**colors_split;
+	int			i;
+
+	i = 0;
+	split = ft_split(line, ' ');
+	if (!split)
+		return (0);
+	while (split[i])
+	{
+		colors_split = ft_split(split[i], ',');
+		if (!colors_split)
+			return (free_split(split));
+		if (colors_split[1])
+		{
+			colors[i] = hex_to_rgba(colors_split[1]);
+			data->default_colors = 0;
+		}
+		else
+			colors[i] = WHITE;
+		free_split(colors_split);
+		i++;
+	}
+	free_split(split);
+	return (1);
+}
+
+static uint32_t	hex_to_rgba(const char *hex)
+{
+	uint32_t	rgb;
+
+	rgb = (uint32_t)ft_strtol(hex, NULL, 16);
+	return (rgb << 8 | 0xFF);
+}
+
+static int	free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+		free(split[i++]);
+	free(split);
+	return (0);
+}
