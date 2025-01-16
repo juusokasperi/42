7f9c22a76c3426b70cd79f91eb6716d15cd989d4
juusokasperi <juusor@gmail.com>
2025-01-16 11:30:53 +0200

    Added security for setting sigaddset, and better error checking for server side signal sending.

--- minitalk/srcs/server.c
+++ minitalk/srcs/server.c
@@ -6,13 +6,13 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 19:49:27 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/01/15 14:43:14 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/01/16 11:29:22 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	ft_signal(int pid, int signal, char *str);
+static void	ft_signal(int pid, int signal, t_buffer *buffer);
 static void	signal_handler(int signum, siginfo_t *info, void *context);
 static void	handle_byte(t_buffer *buffer, int pid);
 
@@ -25,8 +25,9 @@ int	main(void)
 	sa.sa_sigaction = signal_handler;
 	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa.sa_mask, SIGUSR1);
-	sigaddset(&sa.sa_mask, SIGUSR2);
+	if (sigaddset(&sa.sa_mask, SIGUSR1) == -1
+		|| sigaddset(&sa.sa_mask, SIGUSR2) == -1)
+		ft_error("Error setting up sigset.");
 	if (sigaction(SIGUSR1, &sa, NULL) == -1
 		|| sigaction(SIGUSR2, &sa, NULL) == -1)
 		ft_error("Error setting up handlers.");
@@ -60,7 +61,7 @@ static void	signal_handler(int signum, siginfo_t *info, void *context)
 	if (buffer.bit_count == 8)
 		handle_byte(&buffer, pid);
 	else
-		ft_signal(pid, SIGUSR2, buffer.str);
+		ft_signal(pid, SIGUSR2, &buffer);
 }
 
 static void	clear_buffer(t_buffer *buffer)
@@ -86,22 +87,25 @@ static void	handle_byte(t_buffer *buffer, int pid)
 		if (buffer->str)
 			ft_printf("%s\n", buffer->str);
 		clear_buffer(buffer);
-		ft_signal(pid, SIGUSR1, buffer->str);
+		ft_signal(pid, SIGUSR1, buffer);
 		g_pid = 0;
 	}
 	else
-		ft_signal(pid, SIGUSR2, buffer->str);
+		ft_signal(pid, SIGUSR2, buffer);
 	buffer->received_char = 0;
 	buffer->bit_count = 0;
 }
 
-static void	ft_signal(int pid, int signal, char *str)
+static void	ft_signal(int pid, int signal, t_buffer *buffer)
 {
 	if (kill(pid, signal) == -1)
 	{
-		if (str)
-			ft_free((void **)&str);
-		ft_error("Problem with signal transmission.");
+		clear_buffer(buffer);
+		buffer->received_char = 0;
+		buffer->bit_count = 0;
+		ft_printf("%sERROR:%s ", TRED, TRESET);
+		ft_printf("Problem sending acknowledgement. ");
+		ft_printf("State reset, server open for a new connection.\n");
 		g_pid = 0;
 	}
 }
