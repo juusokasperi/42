0283a34f318d11a8bf08501e1c5a55340288a13f
juusokasperi <juusor@gmail.com>
2025-02-17 23:09:15 +0200

    Fixed the docker rules for makefile and fixed philo.c unlock_forks()

--- philosophers/philo/srcs/routine/philo.c
+++ philosophers/philo/srcs/routine/philo.c
@@ -6,7 +6,7 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 22:26:05 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/02/17 15:59:03 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/02/17 18:42:43 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,24 +62,24 @@ static int	p_eat(t_philo *philo)
 {
 	pthread_mutex_lock(philo->left_fork);
 	if (print_msg(philo, "has taken a left fork") == -1)
-		return (unlock_fork(philo->left_fork, NULL, -1));
+		return (unlock_forks(philo->left_fork, NULL, -1));
 	if (philo->data->philo_count == 1)
 	{
 		while (not_dead(philo))
 			ft_usleep(1);
-		return (unlock_fork(philo->left_fork, NULL, 0));
+		return (unlock_forks(philo->left_fork, NULL, 0));
 	}
 	pthread_mutex_lock(philo->right_fork);
 	if (print_msg(philo, "has taken a right fork") == -1)
-		return (unlock_fork(philo->right_fork, philo->left_fork, -1));
+		return (unlock_forks(philo->right_fork, philo->left_fork, -1));
 	pthread_mutex_lock(&philo->meal_mutex);
 	philo->last_meal = get_time_ms() - philo->data->start_time;
 	philo->meals_ate++;
 	pthread_mutex_unlock(&philo->meal_mutex);
 	if (print_msg(philo, "is eating") == -1)
-		return (unlock_fork(philo->right_fork, philo->left_fork, -1));
+		return (unlock_forks(philo->right_fork, philo->left_fork, -1));
 	ft_usleep(philo->data->time_to_eat);
-	return (unlock_fork(philo->right_fork, philo->left_fork, 0));
+	return (unlock_forks(philo->right_fork, philo->left_fork, 0));
 }
 
 static int	p_sleep(t_philo *philo)
