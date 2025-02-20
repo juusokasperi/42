aa92331f0396462481669d62ad1eccffed5ddbda
juusokasperi <juusor@gmail.com>
2025-02-20 17:44:03 +0200

    Removed unnecessary mutex from print_msg

--- philosophers/philo/srcs/utils/print_msg.c
+++ philosophers/philo/srcs/utils/print_msg.c
@@ -6,7 +6,7 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 23:32:15 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/02/20 17:04:33 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/02/20 17:43:05 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,12 @@ int	print_msg(t_philo *philo, char *str)
 {
 	size_t	time;
 
-	pthread_mutex_lock(&philo->data->lock);
 	time = get_time_ms();
 	if (printf("%zu %d %s\n", time - philo->data->start_time, \
 		philo->id, str) == -1)
 	{
 		philo->data->error = 1;
-		pthread_mutex_unlock(&philo->data->lock);
 		return (-1);
 	}
-	pthread_mutex_unlock(&philo->data->lock);
 	return (0);
 }
