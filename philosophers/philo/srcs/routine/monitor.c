96d84faf9c7024fdda2f6809df7ec26e34a25c4a
juusokasperi <juusor@gmail.com>
2025-02-20 13:33:19 +0200

    Uses index i instead of j

--- philosophers/philo/srcs/routine/monitor.c
+++ philosophers/philo/srcs/routine/monitor.c
@@ -6,7 +6,7 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 22:26:08 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/02/20 10:32:22 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/02/20 13:30:12 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,17 +45,17 @@ void	*monitor_routine(void *arg)
 
 static int	check_all_ate_enough(t_philo *philos, t_data *data)
 {
-	int	j;
+	int	i;
 	int	all_ate_enough;
 
-	j = -1;
+	i = -1;
 	all_ate_enough = 1;
-	while (all_ate_enough && ++j < data->philo_count)
+	while (all_ate_enough && ++i < data->philo_count)
 	{
-		pthread_mutex_lock(&data->philos[j].meal_mutex);
-		if (philos[j].meals_ate < data->meals_to_eat)
+		pthread_mutex_lock(&data->philos[i].meal_mutex);
+		if (philos[i].meals_ate < data->meals_to_eat)
 			all_ate_enough = 0;
-		pthread_mutex_unlock(&data->philos[j].meal_mutex);
+		pthread_mutex_unlock(&data->philos[i].meal_mutex);
 	}
 	if (all_ate_enough)
 	{
