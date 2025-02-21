a3e6603c6828efb58e33218b5dddcc25e3b4ef4e
juusokasperi <juusor@gmail.com>
2025-02-21 18:45:01 +0200

    printing uses a lock

--- philosophers/philo/srcs/utils/print_msg.c
+++ philosophers/philo/srcs/utils/print_msg.c
@@ -6,7 +6,7 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 23:32:15 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/02/20 17:43:05 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/02/20 19:26:05 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,20 @@ int	print_msg(t_philo *philo, char *str)
 {
 	size_t	time;
 
+	pthread_mutex_lock(&philo->data->print_lock);
+	if (is_dead(philo) && ft_strcmp(str, "died") != 0)
+	{
+		pthread_mutex_unlock(&philo->data->print_lock);
+		return (-1);
+	}
 	time = get_time_ms();
 	if (printf("%zu %d %s\n", time - philo->data->start_time, \
 		philo->id, str) == -1)
 	{
 		philo->data->error = 1;
+		pthread_mutex_unlock(&philo->data->print_lock);
 		return (-1);
 	}
+	pthread_mutex_unlock(&philo->data->print_lock);
 	return (0);
 }
