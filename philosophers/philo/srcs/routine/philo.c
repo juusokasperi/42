e2a925743364196747ece62d2fa67fb8e01fee25
juusokasperi <juusor@gmail.com>
2025-02-21 18:44:44 +0200

    added wait_for_start function

--- philosophers/philo/srcs/routine/philo.c
+++ philosophers/philo/srcs/routine/philo.c
@@ -6,7 +6,7 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 22:26:05 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/02/20 17:36:12 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/02/21 18:28:40 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 static int		p_think(t_philo *philo);
 static int		p_eat(t_philo *philo);
 static int		p_sleep(t_philo *philo);
-static int		is_dead(t_philo *philo);
+static void		wait_for_start(t_philo *philo);
 
 void	*philo_routine(void *arg)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	wait_for_start(philo);
 	if (philo->id % 2)
 		ft_usleep(philo->data->time_to_eat / 2);
 	while (philo->data->error == 0 && !is_dead(philo))
 	{
 		pthread_mutex_lock(&philo->meal_mutex);
-		while (philo->data->philo_count > 1 && !philo->should_eat_next
-			&& philo->data->error == 0 && !is_dead(philo))
+		while (philo->data->philo_count > 1 && philo->data->error == 0
+			&& !is_dead(philo) && !philo->should_eat_next)
 		{
 			pthread_mutex_unlock(&philo->meal_mutex);
 			ft_usleep(1);
@@ -43,16 +44,22 @@ void	*philo_routine(void *arg)
 	return (arg);
 }
 
-static int	is_dead(t_philo *philo)
+static void	wait_for_start(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->death_lock);
+	pthread_mutex_unlock(&philo->data->death_lock);
+}
+
+int	is_dead(t_philo *philo)
 {
 	int	is_dead;
 
-	pthread_mutex_lock(&philo->data->lock);
+	pthread_mutex_lock(&philo->data->death_lock);
 	if (philo->data->philo_died)
 		is_dead = 1;
 	else
 		is_dead = 0;
-	pthread_mutex_unlock(&philo->data->lock);
+	pthread_mutex_unlock(&philo->data->death_lock);
 	return (is_dead);
 }
 
@@ -67,30 +74,26 @@ static int	p_think(t_philo *philo)
 
 static int	p_eat(t_philo *philo)
 {
-	pthread_mutex_lock(philo->left_fork);
-	if (is_dead(philo))
-		return (unlock_forks(philo->left_fork, NULL, -1));
-	if (print_msg(philo, "has taken a fork") == -1)
-		return (unlock_forks(philo->left_fork, NULL, -1));
+	pthread_mutex_lock(philo->right_fork);
+	if (is_dead(philo) || print_msg(philo, "has taken a fork") == -1)
+		return (unlock_forks(philo->right_fork, NULL, -1));
 	if (philo->data->philo_count == 1)
 	{
 		while (!is_dead(philo))
 			ft_usleep(1);
-		return (unlock_forks(philo->left_fork, NULL, 0));
+		return (unlock_forks(philo->right_fork, NULL, 0));
 	}
-	pthread_mutex_lock(philo->right_fork);
-	if (is_dead(philo))
-		return (unlock_forks(philo->right_fork, philo->left_fork, -1));
-	if (print_msg(philo, "has taken a fork") == -1)
-		return (unlock_forks(philo->right_fork, philo->left_fork, -1));
+	pthread_mutex_lock(philo->left_fork);
+	if (is_dead(philo) || print_msg(philo, "has taken a fork") == -1)
+		return (unlock_forks(philo->left_fork, philo->right_fork, -1));
 	pthread_mutex_lock(&philo->meal_mutex);
 	philo->last_meal = get_time_ms() - philo->data->start_time;
 	philo->meals_ate++;
 	pthread_mutex_unlock(&philo->meal_mutex);
 	if (print_msg(philo, "is eating") == -1)
-		return (unlock_forks(philo->right_fork, philo->left_fork, -1));
+		return (unlock_forks(philo->left_fork, philo->right_fork, -1));
 	ft_usleep(philo->data->time_to_eat);
-	return (unlock_forks(philo->right_fork, philo->left_fork, 0));
+	return (unlock_forks(philo->left_fork, philo->right_fork, 0));
 }
 
 static int	p_sleep(t_philo *philo)
