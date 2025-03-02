b1eb626e853356230f0c9aa0d36be9b94122bf44
juusokasperi <juusor@gmail.com>
2025-03-03 00:02:00 +0200

    Finished work on cpp01

--- cpp-modules/cpp01/ex02/main.cpp
+++ cpp-modules/cpp01/ex02/main.cpp
@@ -6,12 +6,16 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 16:16:19 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/03/02 16:28:09 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/03/02 22:57:25 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <iostream>
-# include <iomanip>
+#include <iostream>
+#include <iomanip>
+
+#define TYELLOW	"\033[0;93m"
+#define TRESET	"\033[0;0m"
+#define SPACER 	"\033[0;93m:\033[0;0m"
 
 int	main(void)
 {
@@ -19,11 +23,13 @@ int	main(void)
 	std::string*	stringPTR = &string;
 	std::string&	stringREF = string;
 
+	std::cout << std::setfill('.') << std::setw(9) << "name" << SPACER
+	<< std::setw(11) << "address" << SPACER << std::setw(16) << "value" << std::endl;
 	std::cout << std::setfill('.') << std::setw(9) << std::right
-	<< "string" << ":" << &string
-	<< ":" << string << std::endl;
-	std::cout << std::setw(9) << std::left << "stringPTR" << ":" << stringPTR
-	<< ":" << *stringPTR << std::endl;
-	std::cout << std::setw(9) << std::left << "stringREF" << ":" << &stringREF
-	<< ":" << stringREF << std::endl;
+	<< "string" << SPACER << &string
+	<< SPACER << string << std::endl;
+	std::cout << std::setw(9) << std::left << "stringPTR" << SPACER << stringPTR
+	<< SPACER << *stringPTR << std::endl;
+	std::cout << std::setw(9) << std::left << "stringREF" << SPACER << &stringREF
+	<< SPACER << stringREF << std::endl;
 }
