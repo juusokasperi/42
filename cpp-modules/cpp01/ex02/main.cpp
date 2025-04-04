faaf7a5ea1c2bda9a9d349ae4e0513bb0c6764cd
jrinta- <juusor@gmail.com>
2025-04-04 16:27:02 +0300

    clearer output

--- cpp-modules/cpp01/ex02/main.cpp
+++ cpp-modules/cpp01/ex02/main.cpp
@@ -6,16 +6,16 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 16:16:19 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/03/03 00:09:41 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/04/04 16:26:36 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <iomanip>
 
-#define TYELLOW	"\033[0;93m"
+#define TYELLOW	"\033[0;33m"
 #define TRESET	"\033[0;0m"
-#define SPACER 	"\033[0;93m:\033[0;0m"
+#define SPACER 	"\033[0;31m : \033[0;0m"
 
 int	main(void)
 {
@@ -24,7 +24,7 @@ int	main(void)
 	std::string&	stringREF = string;
 
 	std::cout	<< std::setfill('.') << std::setw(9) << "name" << SPACER
-				<< std::setw(11) << "address" << SPACER
+				<< std::setw(14) << "address" << SPACER
 				<< std::setw(16) << "value" << std::endl;
 	std::cout	<< std::setfill('.') << std::setw(9) << std::right
 				<< "string" << SPACER << &string
