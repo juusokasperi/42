aff5f4c14241648608343b6bcb9ea8a1be455fea
jrinta- <juusor@gmail.com>
2025-04-13 16:38:57 +0300

    few more test lines

--- cpp-modules/cpp03/ex03/main.cpp
+++ cpp-modules/cpp03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 11:30:07 by jrinta-           #+#    #+#             */
-/*   Updated: 2025/03/07 13:49:56 by jrinta-          ###   ########.fr       */
+/*   Updated: 2025/04/13 16:38:22 by jrinta-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ int	main(void)
 		bill.attack("Test Robot");
 		bill.takeDamage(10);
 	}
+	bill.guardGate();
 	bill.highFivesGuys();
 	bill.beRepaired(10);
+	bill.highFivesGuys();
 	bill.guardGate();
 	bill.whoAmI();
 	return (0);
