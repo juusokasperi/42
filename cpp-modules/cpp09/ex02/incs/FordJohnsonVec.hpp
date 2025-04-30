d220ba3026d6d6cea5443e9c8c5763bceed9cbc7
juusokasperi <juusor@gmail.com>
2025-04-30 15:59:12 +0300

    The algorithm works now, but needs to be restructured to follow the subject guidelines (i.e. classname PmergeMe, maybe using template functions.)

---
+++ cpp-modules/cpp09/ex02/incs/FordJohnsonVec.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FordJohnsonVec.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrinta- <jrinta-@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 17:41:31 by jrinta-           #+#    #+#             */
+/*   Updated: 2025/04/27 20:15:56 by jrinta-          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <chrono>
+
+class FordJohnsonVec {
+	private:
+		static size_t	_comparisons;
+		static std::vector<size_t> generateJacobsthal(size_t n);
+		static std::vector<size_t> calculateInsertionOrder(size_t n);
+		static std::vector<size_t> sortIndices(const std::vector<int> &values);
+		static std::vector<size_t> sortSmall(const std::vector<int> &values, size_t n);
+		static void createPairs(const std::vector<int> &values, std::vector<size_t> &indices,
+			std::vector<size_t> &largerIndices, std::vector<size_t> &smallerIndices,
+			std::vector<size_t> &pairMap, size_t n);
+		static void binaryInsert(std::vector<size_t> &chain,
+			const std::vector<int> &values, size_t indexToInsert,
+			const std::vector<size_t> &pairMap);
+		static size_t jacobsthalNumber(size_t n);
+		static std::vector<size_t> orderToSortedLarger(std::vector<size_t> smallerIndices,
+			std::vector<size_t> largerIndices, std::vector<size_t> sortedLargerIndices);
+	public:
+		static std::vector<int> sort(const std::vector<int> &values);
+		static void displaySorted(const std::vector<int> &values);
+};
---
+++ cpp-modules/cpp09/ex02/srcs/main.cpp
@@ -0,0 +1,82 @@
+#include "FordJohnsonVec.hpp"
+#include "FordJohnsonDeq.hpp"
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <sstream>
+
+static void				numBeforeSorting(std::vector<int> numbers);
+static bool				isPositiveNumber(std::string s);
+static std::vector<int>	pushToVec(int argc, char **argv);
+static std::deque<int>	pushToDeq(std::vector<int> &numsVec);
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (1);
+	try {
+		std::vector<int> numbersVec = pushToVec(argc - 1, argv + 1);
+		std::deque<int> numbersDeq = pushToDeq(numbersVec);
+		numBeforeSorting(numbersVec);
+		FordJohnsonDeq::displaySorted(numbersDeq);
+		FordJohnsonVec::displaySorted(numbersVec);
+	} catch (std::exception &e) {
+		std::cerr << "Error: " << e.what() << "\n";
+	}
+	return (0);
+}
+
+static void	numBeforeSorting(std::vector<int> numbers)
+{
+	std::cout << std::setw(15) << std::left << "Before:" << "[ ";
+	for (size_t i = 0; i < numbers.size(); ++i)
+	{
+		std::cout << numbers[i];
+		if (i + 1 != numbers.size())
+			std::cout << " ";
+	}
+	std::cout << "]\n";
+}
+
+static bool isPositiveNumber(std::string s)
+{
+	size_t i = 0;
+	if (s[0] == '-')
+		return (false);
+	else if (s.length() > 1 && s[0] == '+')
+		i++;
+	while (i < s.length())
+	{
+		if (!std::isdigit(s[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+static std::vector<int>	pushToVec(int argc, char **argv)
+{
+	std::vector<int>	nums;
+
+	for (int i = 0; i < argc; ++i)
+	{
+		if (!isPositiveNumber(argv[i]))
+			throw (std::runtime_error("Invalid input."));
+		try {
+			nums.push_back(std::stoi(argv[i]));
+		} catch (std::exception &e) {
+			throw (std::out_of_range("Integer must be within bounds of 0 to INT_MAX."));
+		}
+	}
+	return (nums);
+}
+
+static std::deque<int>	pushToDeq(std::vector<int> &numsVec)
+{
+	std::deque<int>	nums;
+
+	for (size_t i = 0; i < numsVec.size(); ++i)
+		nums.push_back(numsVec[i]);
+	return (nums);
+}
+
